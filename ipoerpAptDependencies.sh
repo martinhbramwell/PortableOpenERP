f1305b9e62bf66196195241bd95e9c96ae6f3d16
Martin H. J. Bramwell <mhb.warehouseman@gmail.com>
2014-05-08 13:49:16 -0400

    Use aptitude instaed of apt-get 	modified:   ipoerpAptDependencies.sh

--- ipoerpAptDependencies.sh
+++ ipoerpAptDependencies.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 #
-apt-get -y install python ghostscript graphviz libfreetype6-dev python-pdftools
-apt-get -y install libldap2-dev libjpeg-dev libsasl2-dev libpq-dev libxml2 git curl
-apt-get -y install libxslt1-dev libxml2-dev lptools poppler-utils python-pip expect
-apt-get -y install postgresql-client python-dateutil bzr gcc make mc python-dev wget
-apt-get -y install python-imaging python-pychart python-libxslt1 python-matplotlib
-apt-get -y install antiword python-reportlab-accel python-zsi zlib1g-dev zip libffi-dev
+apt-get install aptitude
+#
+aptitude -y install python ghostscript graphviz libfreetype6-dev python-pdftools
+aptitude -y install libldap2-dev libjpeg-dev libsasl2-dev libpq-dev libxml2 git curl
+aptitude -y install libxslt1-dev libxml2-dev lptools poppler-utils python-pip expect
+aptitude -y install postgresql-client python-dateutil bzr gcc make mc python-dev wget
+aptitude -y install python-imaging python-pychart python-libxslt1 python-matplotlib
+aptitude -y install antiword python-reportlab-accel python-zsi zlib1g-dev zip libffi-dev
 #
 mkdir -p ~/tmpxxxxxtmp
 pushd ~/tmpxxxxxtmp
