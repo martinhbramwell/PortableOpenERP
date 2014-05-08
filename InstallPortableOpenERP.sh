e8bbc74b10558f69219cacabec76be3b931767cf
Martin H. J. Bramwell <mhb.warehouseman@gmail.com>
2014-05-08 10:56:41 -0400

    Tidying up  Changes to be committed: 	modified:   InstallPortableOpenERP.sh

--- InstallPortableOpenERP.sh
+++ InstallPortableOpenERP.sh
@@ -31,29 +31,40 @@ then
 #
 echo "01) Fulfill all apt-get dependencis"
 source $DEFDIR/ipoerpAptDependencies.sh
+#
 echo "02) Set hostname"
 source $DEFDIR/iredmailSetHostName.sh
+#
 echo "03) Install all of iRedMail"
 source $DEFDIR/iredmailInstallAll.sh
+#
 echo "04) Prepare users and directories"
 source $DEFDIR/ipoerpPrepareUsersAndDirectories.sh
+#
 echo "05) Generate OpenERP server configuration file"
 source $DEFDIR/ipoerpMakeOerpServerConfigFile.sh
+#
 echo "06) Prepare PostgreSQL User and Tablespace"
 su postgres -c "source $DEFDIR/ipoerpPreparePgUserAndTablespace.sh"
+#
 echo "07) Update OpenERP source code."
 su oerp_user_z -c "source $DEFDIR/ipoerpUpdateOpenErpSourceCode.sh"
+#
 echo "08) Patch OpenERP Launcher"
 su oerp_user_z -c "source $DEFDIR/ipoerpPatchOpenErpLauncher.sh"
+#
 echo "09) Pip install to virtual environment"
 su oerp_user_z -c "source $DEFDIR/ipoerpPipInstallToVEnv.sh"
+#
 echo "10) Make the UPStart script"
 source $DEFDIR/ipoerpMakeUpStartScript.sh
 #
+echo "Finished! A reboot will be required."
+echo "Give it 5 mins, then visit http://${NEWHOSTNAME}.${NEWHOSTDOMAIN}:${ACCESS_PORT}/"
+#
 else
 #
-echo "Done it!"
+echo "Starting partial execution!"
+echo "Partial run complete!"
 #
 fi
-
-
